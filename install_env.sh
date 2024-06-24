ac3cb6ed33d2a9d197a318444d16f5c34259f437
Leung <re2leung@gmail.com>
2024-06-24 11:33:22 +0800

    增加 install_env.sh, 添加 build.sh 可执行权限

---
+++ install_env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Brief        : 
+# Author       : Zen Leung
+# Mail         : re2leung@gmail.com
+# Created Time : Mon 24 Jun 2024 10:28:58 AM HKT
+
+sudo apt update
+sudo apt install build-essential
+sudo apt install libgtest-dev libgmock-dev
+sudo apt install libmosquitto-dev
+sudo apt install libdbus-1-dev
+
