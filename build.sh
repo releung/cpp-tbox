c20a13e88844fa3d987d71d66f61030ff597eaeb
Leung <re2leung@gmail.com>
2025-02-19 19:16:33 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+# cmake 这样编译是编译 lib, 不会编译 example
 cmake -B build -DCMAKE_INSTALL_PREFIX=$HOME/.tbox
 cmake --build build
 cmake --install build
 
+ls $HOME/.tbox 
+#include  lib
+
+# 可以直接 make , 会编译 lib 和 examples
+# ls .install .staging
+# .install:
+# bin  etc  lib
+#
+# .staging:
+# include  lib
